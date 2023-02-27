0ab6158e5afefac8483ece614f2f0107878d5bbe
Md saheb <sahebbca2020@gmail.com>
2023-02-27 18:44:07 +0530

    added slider

--- Js/script.js
+++ Js/script.js
@@ -14,3 +14,19 @@ light.addEventListener('click',()=>{
         visualMode.classList.add("fa-moon");
     }
 });
+
+var slide = document.getElementById("slider");
+var btn1 = document.getElementById("btn-1");
+var btn2 = document.getElementById("btn-2");
+btn1.onclick = function(){
+    slide.style.transform = "translateX(0px)";
+    btn1.classList.add("active");
+    btn2.classList.remove("active");
+}
+btn2.onclick = function(){
+    slide.style.transform = "translateX(-100%)";
+    btn2.classList.add("active");
+    btn1.classList.remove("active");
+}
+
+
